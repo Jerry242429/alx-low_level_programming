646d14d4346b73e672c7167848d0af4498ff23e8
Jerry242429 <eyerusalem008@gmail.com>
2023-10-02 16:15:27 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,78 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
-void check_FIO_status(int status, int file_d, char  *filename, char m);
+char *c_buffer(char *filef);
+void c_file(int file_d);
 
 /**
- * main - copy the content of file to another file
- * @argc: to be argument count variable
- * @argv: to be argument vector variable
- * Return: if on success 1, if not on exit
+ * c_buffer - a function buffer it allocates 1024 bytes
+ * @filef: the name of the file
+ * Return: pointer to aloocated buffer
+ *
+ */
+
+char *c_buffer(char *filef)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", filef);
+		exit(99);
+
+	}
+return (buf);
+
+}
+
+/**
+ * c_file - close opened file using file_d
+ * @file_d: the file descriptor to be closed
+ */
+
+void c_file(int file_d)
+{
+	int a;
+
+	a = close(file_d);
+	if (a == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_d);	
+
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count variable
+ * @argv: argument variable vector
+ * Return: if on sucess 0, otherwise return exit
  *
  */
 
 int main(int argc, char *argv[])
 {
 	int s, d;
-	int cs, cd;
-	int wrote, nread = 1024;
-	char bu[1024];
-	unsigned int m = S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int wrote, nread;
+	char *buf;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buf = c_buffer(argv[2]);
 	s = open(argv[1], O_RDONLY);
-	check_FIO_status(s, -1, argv[1], 'o');
-	d = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, m);
-	check_FIO_status(d, -1, argv[2], 'W');
-	while (nread == 1024)
-	{
-		nread = read(s, bu, sizeof(bu));
-		if (nread == -1)
-			check_FIO_status(-1, -1, argv[1], 'o');
-		wrote = write(d, bu, nread);
-		if (wrote == -1)
-			check_FIO_status(-1, -1, argv[2], 'W');
-	}
-	cs = close(s);
-	check_FIO_status(cs, s, NULL, 'C');
-	cd = close(d);
-	check_FIO_status(cd, d, NULL, 'C');
+	nread = read(s, buf, 1024);
+	d = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	do {
+		if (s == -1 || nread == -1)
+		{
+			dprintf(STDERR_FILENO,  "Error: Can't read from %s\n",  argv[1]);
+
+			free(buf);
+			exit(98);
+		}
+		wrote = write(d, buf, nread);
+		if (d  == -1 || wrote == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+
+		nread = read(s, buf, 1024);
+		d = open(argv[2], O_WRONLY | O_APPEND);
+	} while (nread > 0);
+
+	free(buf);
+	c_file(s);
+	c_file(d);
+
 	return (0);
-}
-
-/**
- * check_I/O_status- that checks file whether opened or closed
- * @status: the file to be opend
- * @filename: the name of the file
- * @m: which is closing or opening
- * @file_d: file desrcriptor of the functiom
- * Return: void
- *
- */
-
-void check_FIO_status(int status, int file_d, char *filename, char m)
-{
-	if (m == 'C' && status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd%d\n", file_d);
-		exit(100);
-	}
-	else if (m == 'W' && status == -1)
-	{
-
-		dprintf(STDERR_FILENO, "Error: cannot write to the %s\n", filename);
-		exit(99);
-	}
-	else if (m == 'o' && status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: cannot be read from file %s\n", filename);
-		exit(98);
-	}
 }
